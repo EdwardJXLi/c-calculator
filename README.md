0b464691707457ad3d9c2ff6d29b1945eb5bd5bb
Edward Li <radioactivehydra@gmail.com>
2021-04-04 09:30:37 -0400

    Cleaned Input

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # C Calculator
 
 A dead simple calculator to experiment with some C tricks
+
+### Stuff Completed For Far
+- pass
--- main.c
+++ main.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
+#include "utils/clean.h"
 
 int main()
 {
-    printf("Hello World!\n");
+    // Create a test input that hopefully tries out all test cases
+    char testInput[] = "   1 + 2 /3 + (1+2-3+4    * 5)";
+
+    // Clean Input
+    removeSpaces(testInput);
+
+    // Print Out Test Cases
+    printf("%s\n", testInput);
 }
